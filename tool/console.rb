41f7229bcaf0b5389fe107698fc85366e974087f
Glen <glen.kimani@student.moringaschool.com>
2023-02-19 19:41:57 +0300

    added

---
+++ tool/console.rb
@@ -0,0 +1,29 @@
+require_relative '../config/environment.rb'
+
+def reload
+  load 'config/environment.rb'
+end
+
+
+###  WRITE YOUR TEST CODE HERE ###
+harrison = Author.new("harrison")
+omondi = Author.new("Omondi")
+vincent = Author.new("vincent")
+
+forbes = Magazine.new("The Forbes", "Awesomeness")
+times = Magazine.new("New times", "News")
+mwanaspoti = Magazine.new("Mwanaspoti", "Sports")
+
+
+
+
+
+
+
+
+
+
+### DO NOT REMOVE THIS
+binding.pry
+
+0
